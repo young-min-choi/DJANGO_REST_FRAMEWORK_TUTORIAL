a563f5e1965cdd29a8686e53d353858ea618c009
YM Choi <archoiym@gmail.com>
2017-03-14 22:37:03 +0900

    POST로 code와 title을 보낸 후 response를 확인하는 테스트 코드 작성.

--- django_app/snippets/tests.py
+++ django_app/snippets/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, APITestCase
 
-# Create your tests here.
+from snippets.models import Snippet
+
+
+class SnippetTests(APITestCase):
+    def test_create_account(self):
+        """
+        Ensure we can create a new snippet object.
+        """
+        url = reverse('snippets:list')
+        data = {'code':'url()',
+                'title': 'test-snippet',
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Snippet.objects.count(), 1)
+        self.assertEqual(Snippet.objects.get().title, 'test-snippet')
--- django_app/snippets/urls.py
+++ django_app/snippets/urls.py
@@ -3,8 +3,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 from snippets import views
 
+app_name = 'snippets'
+
 urlpatterns = [
-    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/$', views.SnippetList.as_view(), name='list'),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 ]
 
